58566ae2d9a2b872e8202b08e592f922f808f3d3
Yash <shrivastavay80@gmail.com>
2022-01-11 21:55:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [weight, setWeight] = useState();
+  const [name, setName] = useState("");
+  const [moves, setMoves] = useState();
+  const [abilities, setAbilities] = useState();
+  const [height, setHeight] = useState();
+  const [species, setSpecies] = useState();
+  const [base, setBase] = useState();
+  const [pname, setPname] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        setMoves(res.data.moves.length);
+        setWeight(res.data.weight);
+        setAbilities(res.data.abilities.length);
+        setHeight(res.data.height);
+        setSpecies(res.data.species.name);
+        setBase(res.data.base_experience);
+    }
+    getData();
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='title'>
+        <h1>FindPoke</h1>
+      </div>
+      <div className="content">
+
+        <h3>Search Pokemon by name</h3>
+        <input type="text" value={pname} onChange={(e) => {
+          setPname(e.target.value);
+        }} />
+        <br />
+        <button className='btn btn-primary' onClick={() => {
+          setName(pname.toLowerCase());
+        }}>Submit </button>
+
+        {name !== "" ?
+          <div className="cards">
+          <div className="card" style={{ width: '18rem' }}>
+            <div class="card-body">
+              <h2 class="card-title">{name}</h2>
+              <div className="list">
+                <ul type='none'>
+                  <li>Moves: {moves}</li>
+                  <li>Weight: {weight}</li>
+                  <li>Abilities: {abilities}</li>
+                </ul>
+                <ul type='none'>
+                  <li>Height: {height}</li>
+                  <li>Species: {species}</li>
+                  <li>Base Experience: {base}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div> : null
+        }
+       
+      </div>
+    </>
+  )
 }
 
 export default App;
